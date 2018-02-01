106ad63c2ff352bfdcdf2c43b8c0f1106bf59da6
Spiffpitt <matthew6174@gmail.com>
2018-02-01 16:47:54 -0600

    Fixed up and reformatted

--- post.php
+++ post.php
@@ -16,18 +16,17 @@ if(isset($_GET['id'])):
 	<body>
 		<section id="container">
 			<?php require 'header.php'; ?>
-				<?php if ($statement->rowCount() > 0) : ?>
-					<?php while ($row = $statement->fetch()) : ?>
-						<section class="post" id='post<?=$statement->rowCount()+1?>'>
-							<h3 class='title'><?=$row['title']?></h3>
-							<small class='date'><?=$row['postdate']?></small>
-							<p><?=$row['post']?></p>
-						</section>
-					<?php endwhile; ?>
-					<?php else : ?>
-						<p>That post does not exist!</p>
-				<?php endif; ?>
-			</section>
+			<?php if ($statement->rowCount() > 0) : ?>
+				<?php while ($row = $statement->fetch()) : ?>
+					<section class="post" id='post<?=$statement->rowCount()+1?>'>
+						<h3 class='title'><?=$row['title']?></h3>
+						<small class='date'><?=$row['postdate']?></small>
+						<p><?=$row['post']?></p>
+					</section>
+				<?php endwhile; ?>
+				<?php else : ?>
+					<p>That post does not exist!</p>
+			<?php endif; ?>
 		</section>
 	</body>
 </html>
