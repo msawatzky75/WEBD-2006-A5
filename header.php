8d1265adade3ff2db15b96046dfcb264b02d84d5
Spiffpitt <matthew6174@gmail.com>
2018-02-02 12:13:10 -0600

    Added header image link to the home page

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <header id="main">
-	<img src="logo.png" alt="Logo">
-	<h1>My Amazing Blog</h1>
+	<a href="index.php" id='home'>
+		<img src="logo.png" alt="Logo">
+		<h1>My Amazing Blog</h1>
+	</a>
 	<nav>
 		<a href="index.php">Home</a>
 		<a href="create.php">New Post</a>
